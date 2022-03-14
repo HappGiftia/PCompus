d2b45b049c0fe86e525f97145ed2a4f7f5590ab2
HappGiftia <157439173@qq.com>
2022-03-14 08:08:13 +0800

    2022/3/12

--- DataModel/crebas.sql
+++ DataModel/crebas.sql
@@ -1,41 +1,101 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2022/3/12 21:19:51                           */
+/* Created on:     2022/3/13 12:00:05                           */
 /*==============================================================*/
 
 
-drop table if exists pcompus.auto_echarts;
-
-alter table pcompus.user
-   drop primary key;
-
-drop table if exists pcompus.tmp_user;
-
-rename table pcompus.user to tmp_user;
-
 /*==============================================================*/
-/* Table: authoiity                                             */
+/* Table: administrator                                         */
 /*==============================================================*/
-create table authoiity
+create table administrator
 (
-   authority_name       varchar(10)
+   admin_id             bigint not null,
+   admin_phone          varchar(11) not null,
+   admin_password       varchar(15) not null,
+   recent_login_time    datetime,
+   primary key (admin_id)
+);
+
+/*==============================================================*/
+/* Table: authority                                             */
+/*==============================================================*/
+create table authority
+(
+   authority_name       varchar(10) not null,
+   primary key (authority_name)
+);
+
+/*==============================================================*/
+/* Table: comment                                               */
+/*==============================================================*/
+create table comment
+(
+   comment_id           bigint not null,
+   comment_type         varchar(8) not null,
+   comment_author       bigint not null,
+   comment_content      varchar(255) not null,
+   create_time          datetime not null,
+   primary key (comment_id)
 );
 
 /*==============================================================*/
 /* Table: echarts                                               */
 /*==============================================================*/
-create table pcompus.echarts
+create table echarts
 (
-   column_id            national varchar(11) not null,
-   title                national varchar(10) not null,
-   echarts_label        national varchar(50) not null,
-   echarts_x            national varchar(50),
-   echarts_series_name  national varchar(50) not null,
-   echarts_series_type  national varchar(8) not null,
-   echarts_series_stack national varchar(50),
-   echarts_series_data  national varchar(255) not null,
-   update_echarts_time  datetime not null,
-   primary key (column_id, title)
+   echarts_id           bigint not null,
+   title                varchar(10) not null,
+   echarts_label        varchar(50) not null,
+   echarts_x            varchar(50),
+   echarts_series_name  varchar(50) not null,
+   echarts_series_type  varchar(8) not null,
+   echarts_series_stack varchar(50),
+   echarts_series_data  varchar(255) not null,
+   updata_echarts_time  datetime not null
+);
+
+/*==============================================================*/
+/* Table: modules_types                                         */
+/*==============================================================*/
+create table modules_types
+(
+   modules_names        varchar(8) not null,
+   primary key (modules_names)
+);
+
+/*==============================================================*/
+/* Table: practice                                              */
+/*==============================================================*/
+create table practice
+(
+   practice_id          bigint not null,
+   practice_name        varchar(20) not null,
+   practice_description varchar(255),
+   practice_type        varchar(20) not null,
+   practice_faculties_range varchar(30),
+   paractice_allowed_range varchar(30),
+   practice_creator     varchar(8) not null,
+   practice_create_time datetime not null,
+   practice_start_time  datetime not null,
+   practice_end_time    datetime not null,
+   practice_cancellate_time datetime,
+   practice_enroll_count int not null,
+   practice_complete_count int not null,
+   practice_bonus_credits real not null,
+   practice_bonus_credits_type varchar(20) not null,
+   is_finished          bool not null,
+   primary key (practice_id, practice_name)
+);
+
+/*==============================================================*/
+/* Table: reply_info                                            */
+/*==============================================================*/
+create table reply_info
+(
+   comment_id           bigint not null,
+   comment_reply_id     bigint not null,
+   reply_time           datetime not null,
+   primary key (comment_id, comment_reply_id)
 );
 
 /*==============================================================*/
@@ -50,15 +110,28 @@ create table user
    user_email           varchar(25) not null,
    user_imge            longblob,
    login_time           datetime,
-   authority            varchar(10),
-   primary key (user_id, user_phoneNumber, user_email)
+   authority            varchar(10) not null,
+   primary key (user_id)
 );
 
-#WARNING: The following insert order will fail because it cannot give value to mandatory columns
-insert into user (user_id, user_name, user_password, user_phoneNumber, user_email)
-select user_id, user_name, user_password, user_phoneNumber, ?
-from pcompus.tmp_user;
+alter table administrator add constraint FK_Reference_7 foreign key (admin_id, admin_password, admin_phone, recent_login_time)
+      references user (user_id, user_password, user_phoneNumber, login_time) on delete restrict on update restrict;
+
+alter table comment add constraint FK_Reference_5 foreign key (comment_author)
+      references user (user_id) on delete restrict on update restrict;
+
+alter table comment add constraint FK_Reference_8 foreign key (comment_type)
+      references modules_types (modules_names) on delete restrict on update restrict;
+
+alter table reply_info add constraint FK_Reference_2 foreign key (comment_reply_id)
+      references comment (comment_id) on delete restrict on update restrict;
+
+alter table reply_info add constraint FK_Reference_3 foreign key (comment_id)
+      references comment (comment_id) on delete restrict on update restrict;
+
+alter table reply_info add constraint FK_Reference_4 foreign key (reply_time)
+      references comment (create_time) on delete restrict on update restrict;
 
 alter table user add constraint FK_Reference_1 foreign key (authority)
-      references authoiity (authority_name) on delete restrict on update restrict;
+      references authority (authority_name) on delete restrict on update restrict;
 
