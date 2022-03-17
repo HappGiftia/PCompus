8873f01a6302c9fba2ff46701681e628be966873
HappGiftia <157439173@qq.com>
2022-03-17 20:02:04 +0800

    2022/3/17

--- PCompus_controller/src/main/java/com/uof/pcompus/filters/testfilter.java
+++ PCompus_controller/src/main/java/com/uof/pcompus/filters/testfilter.java
@@ -1,11 +1,12 @@
 package com.uof.pcompus.filters;
 
 import javax.servlet.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebFilter(filterName = "Filter", urlPatterns = "/testfilter")
+@WebFilter(filterName = "Filter", urlPatterns = "/*")
 public class testfilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
 
@@ -16,9 +17,27 @@ public class testfilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-//        chain.doFilter(request, response);
-//        PrintWriter printWriter = response.getWriter();
-//        printWriter.write("this is filter");
-        System.out.println("this is filter");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        //跨域请求，*代表允许全部类型
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        //允许请求方式
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        //用来指定本次预检请求的有效期，单位为秒，在此期间不用发出另一条预检请求
+        res.setHeader("Access-Control-Max-Age", "3600");
+        //请求包含的字段内容，如有多个可用哪个逗号分隔如下
+        res.setHeader("Access-Control-Allow-Headers", "content-type,x-requested-with,Authorization, x-ui-request,lang");
+        //访问控制允许凭据，true为允许
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        // 浏览器是会先发一次options请求，如果请求通过，则继续发送正式的post请求
+        // 配置options的请求返回
+        if (req.getMethod().equals("OPTIONS")) {
+            res.setStatus(200);
+            res.getWriter().write("OPTIONS returns OK");
+            return;
+        }
+        System.out.println("放行");
+        // 传递业务请求处理
+        chain.doFilter(req, res);
     }
 }
